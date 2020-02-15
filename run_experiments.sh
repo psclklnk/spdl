524ec4c8b43bf6cc64853679bf4eeec79cef33e8
Pascal Klink <pascal.klink@googlemail.com>
2020-02-15 22:10:18 +0100

    Changed running order of experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,9 @@
-python -u run_experiment.py --env point_mass --type default --seed $1
-python -u run_experiment.py --env point_mass --type random --seed $1
 python -u run_experiment.py --env point_mass --type self_paced --seed $1
 python -u run_experiment.py --env point_mass --type alp_gmm --seed $1
+python -u run_experiment.py --env point_mass --type default --seed $1
+python -u run_experiment.py --env point_mass --type random --seed $1
 
-python -u run_experiment.py --env point_mass_2d --type default --seed $1
-python -u run_experiment.py --env point_mass_2d --type random --seed $1
 python -u run_experiment.py --env point_mass_2d --type self_paced --seed $1
 python -u run_experiment.py --env point_mass_2d --type alp_gmm --seed $1
+python -u run_experiment.py --env point_mass_2d --type default --seed $1
+python -u run_experiment.py --env point_mass_2d --type random --seed $1
